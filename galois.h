21a3f6044b9c920c9939b1af2cbe20aabda9c402
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-08-08 10:02:15 +0300

    Update galois.h

--- galois.h
+++ galois.h
@@ -1,1 +1,6 @@
+typedef unsigned char gal8;
 
+typedef uint8_t gal8; /* Galois field of order 2^8 */
+
+const gal8 min_poly  = 0b11101,     /* Minimal polynomial x^8 + x^4 + x^3 + x^2 + 1 */
+           generator = 0b10;        /* Generator of Galois field */
