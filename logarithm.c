bfdd1d9908298af1c1ffcc7b5141ae9991c849c8
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-08-08 10:08:49 +0300

    Added fast multiplication

--- logarithm.c
+++ logarithm.c
@@ -2,9 +2,10 @@
 
 #include "galois.h"
 
-gal8 gal_pow(gal8 b, int e);
-gal8 gal_exp(int n);
-int gal_log(gal8 a);
+gal8 gal_mul_fast(gal8 a, gal8 b); /* Fast multiplication */
+gal8 gal_pow(gal8 b, int e);       /* b ^ e */
+gal8 gal_exp(int n);               /* generator ^ n */
+int gal_log(gal8 a);               /* Return 0 ≤ n ≤ 254 such that generator ^ n = a */
 
 static gal8 exps[255], logs[255]; /* Exponent and logarithm tables linked to each other by way of indices and pointers */
 static int calculated = 0;
@@ -20,6 +21,11 @@ void setup_tables()
         exps[i] = logs + a - 1;
         logs[a - 1] = exps + i;
     }
+}
+
+gal8 gal_mul_fast(gal8 a, gal8 b)
+{
+    return gal_exp(gal_log(a) + gal_log(b));
 }
 
 gal8 gal_pow(gal8 b, int e)
