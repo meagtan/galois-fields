4bfbb4389ddaf214819ee1c3e6ba60cf34dc60fc
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-08-08 10:02:51 +0300

    Added header include

--- galois.c
+++ galois.c
@@ -7,12 +7,7 @@
  */
 
 #include <stdio.h>
-#include <stdint.h>
-
-typedef uint8_t gal8; /* Galois field of order 2^8 */
-
-const gal8 min_poly  = 0b11101,     /* Minimal polynomial x^8 + x^4 + x^3 + x^2 + 1 */
-           generator = 0b10;        /* Generator of Galois field */
+#include "galois.h"
 
 gal8 gal_add(gal8 a, gal8 b);       /* Add two elements of GF(2^8) */
 gal8 gal_mul(gal8 a, gal8 b);       /* Multiply two elements of GF(2^8) */
