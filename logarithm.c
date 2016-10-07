f2e30beec22c7cb4a7d0171095227e82f2145cda
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-10-07 15:16:22 +0300

    Moved declarations to header

--- logarithm.c
+++ logarithm.c
@@ -1,24 +1,6 @@
 /* Memoized discrete exponentiation and logarithm */
 
-#include "galois.h"
-
-extern gal8 gal_mul(gal8 a, gal8 b);
-extern void gal_print(gal8 a);
-
-gal8 gal_mul_fast(gal8 a, gal8 b); /* Fast multiplication */
-gal8 gal_div_fast(gal8 a, gal8 b); /* Fast division */
-
-gal8 gal_pow(gal8 b, int e); /* b ^ e */
-gal8 gal_exp(int n);         /* generator ^ n */
-int gal_log(gal8 a);         /* Return 0 ≤ n ≤ 254 such that generator ^ n = a */
-
-/* Exponent and logarithm tables linked to each other by way of indices */
-static gal8 exps[255];
-static int  logs[255];
-
-static int calculated = 0;
-
-static void setup_tables();
+#include "logarithm.h"
 
 void setup_tables()
 {
