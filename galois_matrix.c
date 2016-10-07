290deb86be56185184414df02c910740e31e5429
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-10-07 15:19:19 +0300

    Moved declarations to header

--- galois_matrix.c
+++ galois_matrix.c
@@ -6,21 +6,7 @@
 
 #include <stdio.h>
 #include <unistd.h> /* for sleep */
-#include "galois.h"
-
-typedef uint8_t gal8;
-             
-const gal8 gener[8] = {1, 0x80, 0x40 + 1, 0x20 + 1, 0x10 + 1, 8, 4, 2}, /* companion matrix of min_poly */
-           gentr[8] = {0x40, 0x20, 0x10, 8, 4, 2, 1, 0b10111000}, /* transpose of gener */
-           unity[8] = {0x80, 0x40, 0x20, 0x10, 8, 4, 2, 1},
-           zero[8]  = {0};
-
-gal8 *mat_add(gal8 *a, gal8 *b);          /* add two matrices */
-gal8 *mat_mul(gal8 *a, gal8 *b);          /* multiply two matrices */
-gal8 *mat_scalar_mul(uint8_t a, gal8 *b); /* multiply matrix by scalar */
-gal8 mat_app(gal8 a, gal8 *b);            /* apply matrix to row vector */
-gal8 *to_matrix(gal8 a);                  /* convert byte into matrix */
-void mat_print(gal8 *a);                  /* print matrix */
+#include "galois_matrix.h"
 
 int main()
 {
