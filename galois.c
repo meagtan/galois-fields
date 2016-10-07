8278331fee40e56da4e7ca18be0e1f0842c84def
Ata Deniz Aydın <meagtan@users.noreply.github.com>
2016-10-07 15:20:40 +0300

    Removed test function

--- galois.c
+++ galois.c
@@ -6,27 +6,7 @@
  * bitwise XOR, but multiplication requires the elimination of powers of x <= 8.
  */
 
-#include <stdio.h>
 #include "galois.h"
-
-int main()
-{
-    int i = 0, c = 0;
-    gal8 a = 1;
-    while (i++ < 256) {
-        gal_print(a);
-        /* printf("%d", hamming_distance(a, gal_mul(a, generator))); */
-        a = gal_mul(a, generator);
-        if (c++ < 7) {
-            putchar(' ');
-        } else {
-            putchar('\n');
-            c = 0;
-        }
-    }
-    if (c)
-        putchar('\n');
-}
 
 gal8 gal_add(gal8 a, gal8 b)
 {
